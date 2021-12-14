1229645f547c42e5cc3b788c62a447aa9b847882
Kim <samouh591@gmail.com>
2021-12-14 13:50:38 +0100

    correction

--- roots/root.js
+++ roots/root.js
@@ -12,15 +12,11 @@ rooter.all('*',(req,res)=>{
 	res.status(404);
 if (req.accepts("html")){
 	res.sendFile(path.join(__dirname,"views","404.html"))
-
 }else if (req.accepts("json")){
-
 	res.json({
-
 		"error":"404 Not found"});
 }else {
 	res.type("txt").send("404 Not found");
-
 }
 }
 );
