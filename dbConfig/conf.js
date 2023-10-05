f96e3314c6d35828ba092bee3acaa19718d06ee6
H@kim <32130257+hakimsa@users.noreply.github.com>
2023-10-05 15:34:35 +0200

    Update conf.js

--- dbConfig/conf.js
+++ dbConfig/conf.js
@@ -1,6 +1,6 @@
 const user="hakim:";
 const pass="Ad1234";
-const cluster="@cluster0-y69ki.mongodb.net/";
-const db="productos";
+const cluster="@cluster0.9pawl.mongodb.net/"; 
+const db="dbtest";
 const serv="mongodb+srv://";
 module.exports={user,pass,cluster,db,serv}
