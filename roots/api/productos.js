22db7e2377c8f9fafdf365303abe9e636ca5b8ac
H@kim <32130257+hakimsa@users.noreply.github.com>
2023-10-05 16:46:14 +0200

    Update productos.js

--- roots/api/productos.js
+++ roots/api/productos.js
@@ -2,10 +2,10 @@ const express=require("express");
 const ProductoController = require('../../controllers/ProductoController');
 const rooter =express.Router();
 const path=require("path");
-rooter.route('/products/api/alls').get(ProductoController.getProductos);
-rooter.route('/products/api/find/:id').get(ProductoController.getProducto);
-rooter.route('products/api/save').post(ProductoController.saveProducto);
-rooter.route('products/api/update/:id').put(ProductoController.updateProducto);
-rooter.route('products/api/delete').delete(ProductoController.deleteProducto);
+rooter.route('/api/v1/productos/all').get(ProductoController.getProductos);
+rooter.route('/api/v1/productos/find/:id').get(ProductoController.getProducto);
+rooter.route('/api/v1/productos/save').post(ProductoController.saveProducto);
+rooter.route('/api/v1/producto/update/:id').put(ProductoController.updateProducto);
+rooter.route('/api/v1/producto/delete').delete(ProductoController.deleteProducto);
                                
 module.exports=rooter;
